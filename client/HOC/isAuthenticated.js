52358625ffe08478d1c4315537d13b3cf9aef599
vivekarora-02 <vivek.arora2024@gmail.com>
2023-09-23 18:51:22 +0530

    Wrote code to check if Authenticated ot not

---
+++ client/HOC/isAuthenticated.js
@@ -0,0 +1,65 @@
+// ReactJS
+import { useEffect, useState } from "react";
+
+// NextJS
+import Router from "next/router";
+
+// Material UI
+// Components
+import Typography from '@mui/material/Typography';
+
+// Other Dependencies
+import Cookies from 'js-cookie';
+
+// Custom
+// Services
+import { getCurrentUser } from "../services/auth";
+// Utils
+import { verifyTokens } from '../utils/auth/verify-tokens';
+
+
+const isAuthenticated = (WrappedComponent) => {
+  return (props) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [hasError, setHasError] = useState(false);
+    const [verified, setVerified] = useState(false);
+
+    useEffect(() => {
+      setIsLoading(true);
+      verifyTokens()
+        .then((isValid) => {
+          if (isValid) {
+            return getCurrentUser();
+          } else {
+            Cookies.remove('accessToken', {
+              path: "/"
+            });
+            Cookies.remove('refreshToken', {
+              path: "/"
+            });
+            Router.replace("/auth/signin");
+          }
+        })
+        .then((user) => {
+          if (!!user) {
+            setVerified(true);
+          }
+        })
+        .catch((err) => setHasError(true))
+        .finally(() => setIsLoading(false));
+    }, []);
+
+    if (hasError) {
+      return <Typography variant="body2" color="error">Something went wrong</Typography>;
+    }
+    if (isLoading) {
+      return <Typography variant="body2" color="text.secondary">Loading...</Typography>;
+    }
+    if (verified) {
+      return <WrappedComponent {...props} />;
+    }
+    return <Typography variant="body2" color="info">You are not signed in. Please signin to continue...</Typography>;
+  };
+};
+
+export default isAuthenticated;
